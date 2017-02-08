03edc79af70ac9028eaa1bc2db1f48700dc41db2
Shadasi <shadasithrowaway@gmail.com>
2017-02-08 11:07:55 -0600

    Changed from loop to taking item ID as input, quit with "quit" command.

--- priceQuery.py
+++ priceQuery.py
@@ -7,38 +7,46 @@ eve_central_base_url = 'http://api.eve-central.com/api/marketstat'
 #tritanium_id = str(sys.argv[1])    # Type ID for Tritanium 
 jita_id = 30000142      # Jita's solar system ID
 
-filename = 'output.txt'
-target = open(filename, 'w')
+#filename = 'output.txt'
+#target = open(filename, 'w')
+#target.write("JITA PRICES:  \n\n")
 
-target.write("JITA PRICES:  \n\n")
-for x in range(34, 41):
-    result = ''
-    if x == 34:
-        result = 'Tritatnium'
-    elif x == 35:
-        result = 'Pyerite'
-    elif x == 36:
-        result = 'Mexallon'
-    elif x == 37:
-        result = 'Isogen'
-    elif x == 38:
-        result = 'Nocxium'
-    elif x == 39:
-        result = 'Zydrine'
-    else:
-        result = 'Megacyte'
+#for x in range(34, 41):
+    #result = ''
+    #if x == 34:
+    #    result = 'Tritatnium'
+    #elif x == 35:
+    #    result = 'Pyerite'
+    #elif x == 36:
+    #    result = 'Mexallon'
+    #elif x == 37:
+    #    result = 'Isogen'
+    #elif x == 38:
+    #    result = 'Nocxium'
+    #elif x == 39:
+    #    result = 'Zydrine'
+    #else:
+    #    result = 'Megacyte'
     
-    target.write(result)
-    target.write("\n")
+    #target.write(result)
+    #target.write("\n")
 
+    #target.write("Max Unit Price (Buy Order):  ")
+    #target.write(buy_max)
+    #target.write("\nMedian Unit Price (Sell Order):  ")
+    #target.write(sell_median)
+    #target.write ("\nVolume in System:  ")
+    #target.write(sell_vol)
+    #target.write("\n\n")
+
+def query(item_id, system_id):
     payload = {
-    'typeid': x,
-    'usesystem': jita_id,
+        'typeid': item_id,
+        'usesystem': system_id,
     }
 
-    r = requests.post(eve_central_base_url, data=payload)
-
-    response = r.text
+    req = requests.post(eve_central_base_url, data=payload)
+    response = req.text
 
     tree = ET.fromstring(response)
     marketstat = tree.find('marketstat')
@@ -49,12 +57,13 @@ for x in range(34, 41):
     sell_vol = sell.findtext('volume')
     sell_median = sell.findtext('median')
 
-    target.write("Max Unit Price (Buy Order):  ")
-    target.write(buy_max)
-    target.write("\nMedian Unit Price (Sell Order):  ")
-    target.write(sell_median)
-    target.write ("\nVolume in System:  ")
-    target.write(sell_vol)
-    target.write("\n\n")
+    print('Item ID:            ', item_id)
+    print('Median Sell Price:  ', sell_median)
+    print('Sell Volume:        ', sell_vol)
 
-target.close()
+input_id = input("Enter an item ID:  ")
+while(input_id != 'quit'):
+    query(input_id, jita_id)
+    input_id = input("Enter an item ID:  ")
+
+#target.close()
