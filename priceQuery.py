023d015c0d340fbafc582113513626b13eafeff9
Shadasi <shadasithrowaway@gmail.com>
2017-02-09 10:41:55 -0600

    Cleaned up code, I still want to see old comments in master for nwo

--- priceQuery.py
+++ priceQuery.py
@@ -5,40 +5,7 @@ import csv
 
 eve_central_base_url = 'http://api.eve-central.com/api/marketstat'
 
-#tritanium_id = str(sys.argv[1])    # Type ID for Tritanium 
 jita_id = 30000142      # Jita's solar system ID
-
-#filename = 'output.txt'
-#target = open(filename, 'w')
-#target.write("JITA PRICES:  \n\n")
-
-#for x in range(34, 41):
-    #result = ''
-    #if x == 34:
-    #    result = 'Tritatnium'
-    #elif x == 35:
-    #    result = 'Pyerite'
-    #elif x == 36:
-    #    result = 'Mexallon'
-    #elif x == 37:
-    #    result = 'Isogen'
-    #elif x == 38:
-    #    result = 'Nocxium'
-    #elif x == 39:
-    #    result = 'Zydrine'
-    #else:
-    #    result = 'Megacyte'
-    
-    #target.write(result)
-    #target.write("\n")
-
-    #target.write("Max Unit Price (Buy Order):  ")
-    #target.write(buy_max)
-    #target.write("\nMedian Unit Price (Sell Order):  ")
-    #target.write(sell_median)
-    #target.write ("\nVolume in System:  ")
-    #target.write(sell_vol)
-    #target.write("\n\n")
 
 def query(item_id, system_id):
     payload = {
@@ -67,7 +34,6 @@ def find_id(item_name):
         reader = csv.reader(f)
         for i, row in enumerate(reader):
             if item_name == row[1]:
-                print(row[1], row[2])
                 return row[2]
         print('Not Found')
         sys.exit()
@@ -80,7 +46,3 @@ while(input_id != 'quit'):
     found_id = find_id(input_id)
     query(found_id, jita_id)
     input_id = input("Enter an item Name:  ")
-
-#find_id('Tritanium')
-
-#target.close()
