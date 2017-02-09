80843f36bdd41bfd9892031de5ef574c595f1e9d
Shadasi <shadasithrowaway@gmail.com>
2017-02-09 10:12:57 -0600

    Updated to query by name

--- priceQuery.py
+++ priceQuery.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import requests
 import sys, getopt
+import csv
 
 eve_central_base_url = 'http://api.eve-central.com/api/marketstat'
 
@@ -59,11 +60,27 @@ def query(item_id, system_id):
 
     print('Item ID:            ', item_id)
     print('Median Sell Price:  ', sell_median)
-    print('Sell Volume:        ', sell_vol)
+    print('Sell Volume:        ', sell_vol, '\n')
 
-input_id = input("Enter an item ID:  ")
+def find_id(item_name):
+    with open('typeids.csv', 'r') as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
+            if item_name == row[1]:
+                print(row[1], row[2])
+                return row[2]
+        print('Not Found')
+        sys.exit()
+
+
+input_id = input("Enter an item Name:  ")
+
+
 while(input_id != 'quit'):
-    query(input_id, jita_id)
-    input_id = input("Enter an item ID:  ")
+    found_id = find_id(input_id)
+    query(found_id, jita_id)
+    input_id = input("Enter an item Name:  ")
+
+#find_id('Tritanium')
 
 #target.close()
